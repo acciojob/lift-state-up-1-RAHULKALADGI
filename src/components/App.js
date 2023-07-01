05a3cb80527cebce0cbf5ad149441698c4f3841c
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-01 23:40:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = ()=> {
+    let [show , setShow] = useState(false);
+    return (
+        <div>
+            <div className="parent">
+                <h1>Parent Component</h1> 
+
+            <div className="child">
+                <h2>Child Component</h2>
+                <button onClick={()=>setShow(true)}>Show Modal</button>
+                {
+                    show && (
+                        <div>
+                        <h3>Modal Content</h3>
+                        <h4>This is the modal content</h4>
+                        </div>
+                        
+                    )
+                }
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
